18691d1abe6b9cae82b9642f4b3b1c0c63a9eb8e
Ronaldo Rodrigues Godoi <anyblueangel3@gmail.com>
2024-06-23 03:59:33 -0300

    Atualizando conteúdo de programas Java

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " +smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " +smartTv.volume);
+
+        smartTv.diminuirVolume();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " +smartTv.volume);
+
+        smartTv.aumentarVolume();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " +smartTv.volume);
+
+    }
+
+}
